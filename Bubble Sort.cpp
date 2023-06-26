d50aedf286f5bf9f612451bf94828fa93c8562c6
Aditya Jain <adityajaincb@gmail.com>
2023-06-26 09:37:04 +0530

    buuble sort

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -6,7 +6,7 @@ void BubbleSort(std::vector<int>&Numbers){
     int len = Numbers.size();
 
     for(auto i =0;i<len;i++){
-        for(auto j =0; j<len;j++){
+        for(auto j =0; j<len-i-1;j++){
             if(Numbers[j]>Numbers[j+1]){
                 std::swap(Numbers[j],Numbers[j+1]);
             }
@@ -16,7 +16,7 @@ void BubbleSort(std::vector<int>&Numbers){
 }
 
 
-int main(){
+int main() {
 
     std::vector<int>Numbers ={10,9,8,11,3,4,2,1};
     BubbleSort(Numbers);
